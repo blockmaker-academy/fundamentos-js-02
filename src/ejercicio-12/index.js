6ece65aff9ab0b9fc3e5ec8b2643531b91a5f5bc
juagilei <juagilei@gmail.com>
2023-09-26 09:31:46 +0200

    ejercicio 12 completado

--- src/ejercicio-12/index.js
+++ src/ejercicio-12/index.js
@@ -1,1 +1,9 @@
 // Escribe tu código aquí:
+// Scope de Bloque: Utiliza let para declarar una variable dentro de un bloque (if, for, etc.). Intenta acceder a esa variable fuera del bloque y observa si puedes acceder a ella.
+
+for (let i=0;i=10;i++){
+    let variable = 2
+}
+suma = variable + 2
+console.log(suma)
+
