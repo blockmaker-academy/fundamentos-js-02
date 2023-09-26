9572eea7b5415bc778541e09c9e7f7a23ea04670
juagilei <juagilei@gmail.com>
2023-09-26 08:35:23 +0200

    Ejercicio 2 completado

--- src/ejercicio-02/index.js
+++ src/ejercicio-02/index.js
@@ -1,1 +1,6 @@
-// Escribe tu código aquí:
+// Bucle Do...While: Utiliza un bucle do...while para pedir al usuario que ingrese un número positivo. Continúa pidiendo hasta que ingrese un número positivo.
+let numero
+do {
+  numero = parseInt(prompt('Introduce un numero'))
+} while (numero <=0)
+console.log(numero)
