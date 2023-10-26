cd907ec2344ec4446cf6bba667ed0a6858e519d5
isapsanch <isabelpsanch@hotmail.com>
2023-10-26 21:45:09 +0200

    Añadir Ejercicio 13

--- src/ejercicio-13/index.js
+++ src/ejercicio-13/index.js
@@ -1,1 +1,17 @@
 // Escribe tu código aquí:
+
+function contadorClosures() {
+    let contador = 0
+
+    return function () {
+        contador++
+        return contador
+    }
+}
+
+const incrementar = contadorClosures()
+console.log(incrementar())
+console.log(incrementar())
+
+    
+
