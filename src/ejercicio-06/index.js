293025ab661123583ad35d0a13222740254c1d2a
XaviBarragan <xbarragan73@gmail.com>
2023-10-26 22:51:06 +0200

    Añadir ejercicio-06

--- src/ejercicio-06/index.js
+++ src/ejercicio-06/index.js
@@ -1,1 +1,9 @@
-// Escribe tu código aquí:
+// Ejercicio 6: Funciones
+
+// Crea una función llamada saludar que acepte un nombre como parámetro y retorne un mensaje de saludo.
+function saludar(nombre) {
+    return '¡Hola, ' + nombre + '!'
+}
+
+console.log(saludar('Ada Colau'))
+
