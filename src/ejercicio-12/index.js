544a0a3dbc305c85fec1f8d9460f48e74834a904
XaviBarragan <xbarragan73@gmail.com>
2023-10-26 23:25:48 +0200

    Añadir ejercicio-12

--- src/ejercicio-12/index.js
+++ src/ejercicio-12/index.js
@@ -1,1 +1,9 @@
-// Escribe tu código aquí:
+// Ejercicio 12: Scope de Bloque
+
+// Utiliza let para declarar una variable dentro de un bloque (if, for, etc.). Intenta acceder a esa variable fuera del bloque y observa si puedes acceder a ella.
+if (true) {
+    let variableBloque = 'Variable dentro del bloque'
+}
+
+console.log(variableBloque)
+// ReferenceError: variableBloque is not defined
