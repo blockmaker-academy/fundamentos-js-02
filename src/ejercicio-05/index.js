9c978a3d52ab68f96c2f8f31c5d674be8ab58104
zori13 <zori.gon@mail.com>
2023-11-05 22:42:25 +0100

    Ejercicio 05

--- src/ejercicio-05/index.js
+++ src/ejercicio-05/index.js
@@ -1,1 +1,10 @@
 // Escribe tu código aquí:
+let casa = {
+    color: 'marrón',
+    metros: 360,
+    habitaciones: 6,
+    baños: 4
+}
+for (const caracteristica in casa){
+    console.log(caracteristica + ':' + casa[caracteristica])
+}
